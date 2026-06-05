819445e2202b0569c432a5af4c062294a62a16dd
LucasXMS <lucasxa993304@gmail.com>
2026-06-05 00:24:07 -0300

    Main solucao-padrao-gof

--- src/adapterPOOA/Main.java
+++ src/adapterPOOA/Main.java
@@ -2,17 +2,23 @@ package adapterPOOA;
 
 import java.util.Scanner;
 
+/**
+ * Classe principal do sistema. Atua como o Cliente (Client) no padrão Adapter,
+ * interagindo apenas com a interface abstrata {@link MediaPlayer} sem precisar
+ * conhecer as implementações internas de cada player.
+ */
+
 public class Main {
 
 	public static void main(String[] args) {
-		PlayerMP3 processador = new PlayerMP3();
+		MediaPlayer processador = new MediaAdapter();
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("---Player de Música---");
 		System.out.println("Digite a extensão do arquivo da sua música: ");
 		String arquivo = sc.nextLine();
 		
-		processador.tocarMusicaMP3(arquivo);
+		processador.tocarMusica(arquivo);
 		
 		sc.close();
 
